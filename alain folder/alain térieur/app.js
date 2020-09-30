62382736ae6676fb1b712c06cd598205b8874fa7
dayuum <muscat13270@gmail.com>
2020-09-30 15:06:35 +0200

    modif/ajout du js page article dans alain folder

---
+++ alain folder/alain térieur/app.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.agent', {delay:800, duration:1000, origin: 'right', distance: '60px' } );
+ScrollReveal().reveal('.circulation', {delay:800, duration:1000, origin: 'left', distance: '60px'} );
+ScrollReveal().reveal('#titre2', {delay:800, duration:1000, origin: 'top', distance: '60px'} );
+ScrollReveal().reveal('#titre3', {delay:800, duration:1000, origin: 'bottom', distance: '60px'} );
